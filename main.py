b47de43dc55525210d76b9839af2e1a3e356cfd8
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-17 17:08:25 +0000

    vowels/consonants

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 userInput = str(input("Enter a phrase..."))
 newInput = userInput.lower()
 print(newInput)
+
+vowels = ["a", "e", "i", "o", "u"]
+for i in newInput:
+  if i in vowels: 
+    print (i + "is a vowel")
+else:
+      print(i + "is a consonant")
