dde19c220c25d8d44e8e2365d6d7debbbf5fa047
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-17 17:40:37 +0000

    vowel list

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ userInput = str(input("Enter a phrase..."))
 newInput = userInput.lower()
 print(newInput)
 
-vowels = ["a", "e", "i", "o", "u"]
-for i in newInput:
-  if i in vowels: 
-    print (i + "is a vowel")
-else:
-      print(i + "is a consonant")
+vowels = ["a", "e", "i", "o","u"]
+numberVowels = 0 
+numberConsonants = 0 
+for i in newInput: 
+  if i in vowels: #check the vowel list 
+    print(i + " is a vowel")
+  numberVowels = numberVowels + 1 
+else: 
+    print(i + " is a consonant")
+    numberConsonants = numberConsonants + 1 
+    print("# of vowels:" + str(numberVowels))
+    print("# of consonants:" + str(numberConsonants))
